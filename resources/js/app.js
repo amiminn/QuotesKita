a0e21a3f58318ee1c219f2babae9287fc638fb7e
amiminn <tholabul.amin@gmail.com>
2023-09-06 18:48:33 +0700

    update

---
+++ resources/js/app.js
@@ -0,0 +1,14 @@
+import { createApp, h } from "vue";
+import { createInertiaApp } from "@inertiajs/vue3";
+
+createInertiaApp({
+    resolve: (name) => {
+        const pages = import.meta.glob("./pages/**/*.vue", { eager: true });
+        return pages[`./pages/${name}.vue`];
+    },
+    setup({ el, App, props, plugin }) {
+        const app = createApp({ render: () => h(App, props) });
+        app.use(plugin);
+        app.mount(el);
+    },
+});
---
+++ resources/js/pages/index.vue
@@ -0,0 +1,7 @@
+<template lang="">
+    <he>Index</he>
+</template>
+<script>
+// console.log("app");
+</script>
+<style lang=""></style>
